53b3b731ea8038b0a9ba7f63b0c12123f334f5dd
Matt Pindell <pindell.m@gmail.com>
2016-04-02 16:49:55 -0600

    Routes and views working for iter 7.

--- app/models/url_statistics.rb
+++ app/models/url_statistics.rb
@@ -8,7 +8,7 @@ class UrlStatistics
 
   def initialize(identifier, relativepath)
     @identifier   = identifier
-    @relativepath = relativepath
+    @relativepath = "/" + relativepath
     @client       = Client.find_by(identifier: identifier)
     @view         = render_url_stats_view
   end
@@ -18,20 +18,21 @@ class UrlStatistics
       :url_does_not_exist
     else
       @url = client.urls.find_by(path: relativepath)
-      :show
+      :show_url_statistics
     end
   end
 
   def url_does_not_exist?
-    !@client.urls.exists?(path: relativepath)
+
+    !client.urls.exists?(path: relativepath)
   end
 
-  def url_data
+  def data
     {
       max_response_time:           url.max_response_time,
       min_response_time:           url.min_response_time,
       all_response_times:          url.all_response_times,
-      average_response_times:      url.average_reponse_times.to_i,
+      average_response_times:      url.average_response_time.to_i,
       find_verbs:                  url.find_verbs,
       top_referrers:               url.top_referrers,
       top_user_agents:             url.top_user_agents,
