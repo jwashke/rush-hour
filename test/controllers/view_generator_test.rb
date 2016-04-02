e4dfd3f90d8ef1ed1300bb813928f2f3de5a0ca9
Josh Washke <jwashke@icloud.com>
2016-04-02 15:11:17 -0600

    fixing merge conflict

--- test/controllers/view_generator_test.rb
+++ test/controllers/view_generator_test.rb
@@ -8,23 +8,30 @@ class ViewGeneratorTest < Minitest::Test
     RushHour::Server
   end
 
-  def test_returns_success_post_valid_payload_request
-    post '/sources', {identifier: 'jumpstartlab', rootUrl: 'http://jumpstartlab.com' }
-    assert_equal 1, Client.count
-    assert_equal 200, last_response.status
-    assert_equal "{\"identifier\":\"jumpstartlab\"}", last_response.body
+  def payload_request
+    "payload={\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com/\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\":\"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
+  end
 
-    post '/sources/jumpstartlab/data', "payload={\"url\":\"http://jumpstartlab.com/blog\",\"requestedAt\":\"2013-02-16 21:38:28 -0700\",\"respondedIn\":37,\"referredBy\":\"http://jumpstartlab.com/\",\"requestType\":\"GET\",\"parameters\":[],\"eventName\":\"socialLogin\",\"userAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17\",\"resolutionWidth\":\"1920\",\"resolutionHeight\":\"1280\",\"ip\":\"63.29.38.211\"}"
+  def test_it_returns_show_page_for_valid_client
+    create_client
+    post '/sources/jumpstartlabs/data', payload_request
 
-    get '/sources/jumpstartlab'
+    get '/sources/jumpstartlabs'
 
-    # require "pry"
-    # binding.pry
+    assert last_response.body.include?("Jumpstartlabs")
+  end
 
-    # assert_equal 1, PayloadRequest.count
-    # assert_equal "jumpstartlab", PayloadRequest.first.client.identifier
-    # assert_equal 200, last_response.status
-    # assert_equal "Payload successfully created.\n", last_response.body
+  def test_it_returns_show_page_for_invalid_client
+    get '/sources/notvalidindustries'
+
+    assert last_response.body.include?("Client does not exist")
+  end
+
+  def test_it_returns_show_page_for_invalid_client
+    create_client
+    get '/sources/jumpstartlabs'
+
+    assert last_response.body.include?("Client has no requests")
   end
 
 end
