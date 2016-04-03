9b9371b162f8be988e56b98c84ebdc479d490836
Matt Pindell <pindell.m@gmail.com>
2016-04-03 10:58:15 -0600

    modified event_statistics to show full 24 hours

--- app/models/event_statistics.rb
+++ app/models/event_statistics.rb
@@ -15,7 +15,7 @@ class EventStatistics
 
   def breakdown_by_hour
     # pr.group(:requested_at).count
-    hour_freq = pr.each_with_object(Hash.new(0)) do |preq, hash|
+    pr.map.with_object(initialize_hour_hash) do |preq, hash|
       hash[Time.parse(preq.requested_at).hour] += 1
     end
   end
@@ -33,5 +33,32 @@ class EventStatistics
       :event_statistics
     end
   end
+
+  def initialize_hour_hash
+    { 0  => 0,
+      1  => 0,
+      2  => 0,
+      3  => 0,
+      4  => 0,
+      5  => 0,
+      6  => 0,
+      7  => 0,
+      8  => 0,
+      9  => 0,
+      10 => 0,
+      11 => 0,
+      12 => 0,
+      13 => 0,
+      14 => 0,
+      15 => 0,
+      16 => 0,
+      17 => 0,
+      18 => 0,
+      19 => 0,
+      20 => 0,
+      21 => 0,
+      22 => 0,
+      23 => 0 }
+    end
 
 end
